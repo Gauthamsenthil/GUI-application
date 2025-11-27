5e0f27a5b6ff2cb9cbe3f68dfbfb7f6575dd8dbf
Gauthamsenthil <gauthamsenthilraja@gmail.com>
2025-11-27 21:22:17 +0530

    submit

---
+++ proj3.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+window = Tk()
+window.title("Length Converter App")
+window.geometry('400x400')
+
+label1 = Label(text="give the length (in inches)")
+label1_entry = Entry(window)
+label1.pack()
+label1_entry.pack()
+
+label3 = Label(text="")
+label3.pack()
+
+def convert(event = None):
+    inches = float(label1_entry.get())
+    centimeters = inches * 2.54
+    label3.config(text=f"Inches to cm: {centimeters} cm")
+    
+
+window.bind("<Return>", convert)
+
+button = Button(text="Convert", command=convert)
+button.pack()
+
+window.mainloop()
